dcb4a1070d190ec30e29d130d26a8393423f2191
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:27:50 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
-
+export const CONFIG = {
+    API_URL: "SUA_URL",
+    VERSAO: "5.0.0",
+    EMAIL_LIST: ["gmail.com","hotmail.com","outlook.com","yahoo.com","pm.ro.gov.br"],
+    ADMIN_EMAIL: "1cia.rondon@gmail.com"
+};
