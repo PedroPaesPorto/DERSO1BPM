464facaa5348a4b9f3d37875944bd61a350ea87b
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:28:30 -0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,22 @@
+import { CONFIG } from "./config.js";
+
+export async function carregarDadosIniciais() {
+    const [dResp, lResp] = await Promise.all([
+        fetch(`${CONFIG.API_URL}?action=datas`).then(r => r.json()),
+        fetch(`${CONFIG.API_URL}?action=lista`).then(r => r.json())
+    ]);
+    return { datas: dResp, lista: lResp };
+}
+
+export async function enviarFormulario(formData) {
+    return fetch(CONFIG.API_URL, {
+        method: "POST",
+        body: formData
+    }).then(r => r.json());
+}
+
+export async function buscarHistorico(matricula) {
+    return fetch(`${CONFIG.API_URL}?action=historico&matricula=${matricula}`)
+        .then(r => r.json());
+}
 
