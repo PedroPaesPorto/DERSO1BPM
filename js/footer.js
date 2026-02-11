2359dc2764bcd9fcb46d73ca961066cf03696a44
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:41:00 -0400

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,4 @@
-export function updateFooter() { ... }
+export function updateFooter() {
+        const now = new Date();
+        DOM.footer.textContent = `Desenvolvido na 1ª Cia do 1º BPM pelo PVSA Pedro Porto - versão ${CONFIG.VERSAO} - em ${(now.getMonth() + 1).toString().padStart(2, '0')}/${now.getFullYear()}`;
+    }
