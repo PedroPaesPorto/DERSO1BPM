5dddc46b0878ac0443694c244ee45624f2560d38
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:29:13 -0400

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,11 @@
+import { STATE } from "./state.js";
+
+export function abrirPainelAdmin() {
+    alert(
+        "LOGS DA SESSÃO\n\n" +
+        STATE.sessionLogs.map(l =>
+            `${l.data} - ${l.acao} - ${l.tipo}`
+        ).join("\n")
+    );
+}
 
