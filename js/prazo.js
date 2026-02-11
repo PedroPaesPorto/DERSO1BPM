2258af3ed55c11944aacd74cef38042f8264a6fe
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:28:57 -0400

    Update prazo.js

--- js/prazo.js
+++ js/prazo.js
@@ -1,1 +1,12 @@
+export function calcularTempoRestante(fechamento) {
+    const agora = Date.now();
+    const diff = fechamento - agora;
+
+    const d = Math.floor(diff / 86400000);
+    const h = Math.floor((diff % 86400000) / 3600000);
+    const m = Math.floor((diff % 3600000) / 60000);
+    const s = Math.floor((diff % 60000) / 1000);
+
+    return { diff, d, h, m, s };
+}
 
