0b7406519ac492a655c33cb79af30b2ff6d95ce2
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 10:30:31 -0400

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -108,7 +108,7 @@ export function applyInstitutionalTheme(matriculaLogada = null) {
         7: "👔 Agosto: Família é alicerce da missão profissional.",
         8: "🇧🇷 Setembro: Renovamos nosso juramento de servir e proteger.",
         9: "🎗️ Outubro: Prevenção é compromisso com a vida.",
-        10: "📜 Novembro: Compromisso com os ideais republicanos. ",
+        10: "📜 Novembro: Compromisso com os ideais republicanos. ${hoje.getFullYear() - 1983} º ano da República. ",
         11: "🎄 Dezembro: Planejamento garante um final de ano seguro."
     };
 
