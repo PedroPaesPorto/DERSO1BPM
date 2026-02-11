06f60cf2f97951ca911659e8688c803a8e954f4e
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:29:34 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+import { carregarDadosIniciais } from "./api.js";
+import { STATE } from "./state.js";
+
+async function init() {
+    try {
+        const dados = await carregarDadosIniciais();
+        STATE.employeeList = dados.lista;
+        console.log("Sistema 5.0 iniciado");
+    } catch (e) {
+        console.error("Erro crítico:", e);
+    }
+}
+
+init();
 
