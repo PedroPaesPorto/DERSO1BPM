d71f7d5246bd9fe3e6344bcdf2f5f9b990447597
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:48:51 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,9 @@
-export function showModal(title, text, icon, color, showHistory = false) { ... }
+export  function showModal(title, text, icon, color, showHistory = false) {
+        document.getElementById('modalTitle').textContent = title;
+        document.getElementById('modalText').textContent = text;
+        const iconDiv = document.getElementById('modalIcon');
+        iconDiv.textContent = icon;
+        iconDiv.style.color = color;
+        DOM.historyContent.classList.toggle('is-hidden', !showHistory);
+        DOM.modal.style.display = 'flex';
+    }
