6e2744784a8b56bbe7b84e5b3b953797de30f1f1
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-11 09:28:44 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,8 @@
+export function salvarRascunho(dados) {
+    localStorage.setItem("dersoDraft", JSON.stringify(dados));
+}
+
+export function restaurarRascunho() {
+    return JSON.parse(localStorage.getItem("dersoDraft"));
+}
 
